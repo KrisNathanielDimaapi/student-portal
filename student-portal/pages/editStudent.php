79b49e5b46287e7db3269ac91e3eeac106ee17d6
John Dexter Lanot <21-09401@g.batstate-u.edu.ph>
2024-02-15 06:13:10 -0800

    update student working

--- student-portal/pages/editStudent.php
+++ student-portal/pages/editStudent.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $host = "localhost";
@@ -11,29 +10,35 @@ if ($connect->connect_error) {
     die("Error Connect to DB" . $connect->connect_error);
 }
 
-
 $firstname = "";
-$middlename= "";
-$lastname= "";
-$address= "";
-$class_id= "";
+$middlename = "";
+$lastname = "";
+$address = "";
+$class_id = "";
 
-if($_SERVER['REQUEST_METHOD'] == 'GET'){
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-    if(!isset($_GET['id'])){
+    if (!isset($_GET['id'])) {
         header("location: student.php");
         exit;
     }
 
     $id = $_GET['id'];
 
-    $sql = "SELECT * FROM students WHERE id = '$id'";
+    $sql = "SELECT * FROM students WHERE id = ?";
+    $stmt = $connect->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    $result = $connect->query($sql);
+    if (!$result) {
+        die("Error executing query: " . $stmt->error);
+    }
+
     $row = $result->fetch_assoc();
     if (!$row) {
-        die("Error Get Data ");
         header('location: student.php');
+        exit;
     }
 
     $id = $row["id"];
@@ -43,22 +48,17 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
     $address = $row["address"];
     $class_id = $row["class_id"];
 
-}else{
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    $id = $_POST["id"];
-    $firstname = $_POST["firstname"];
-    $middlename = $_POST["middlename"];
-    $lastname = $_POST["lastname"];
-    $address = $_POST["address"];
-    $class_id = $_POST["class_id"];
+    $id = isset($_POST['id']) ? $_POST['id'] : '';
+    $firstname = isset($_POST['firstname']) ? $_POST['firstname'] : '';
+    $middlename = isset($_POST['middlename']) ? $_POST['middlename'] : '';
+    $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : '';
+    $address = isset($_POST['address']) ? $_POST['address'] : '';
+    $class_id = isset($_POST['class_id']) ? $_POST['class_id'] : '';
 
-    if($firstname == "" || $middlename =="" || $lastname == "" || $address == "" || $class_id == ""){
-
-        echo "
-            <script>
-                alert('All Field Can Not Empty');
-            </script>
-        ";
+    if (empty($firstname) || empty($middlename) || empty($lastname) || empty($address) || empty($class_id)) {
+        echo "<script>alert('All fields cannot be empty');</script>";
         die();
     }
 
@@ -66,19 +66,16 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
     $stmt = $connect->prepare($sql);
     $stmt->bind_param("sssssi", $firstname, $middlename, $lastname, $address, $class_id, $id);
     $stmt->execute();
-    
-    if (!$stmt) {
-        echo  " <script>
-            alert('Edit Not Success !');
-        </script> ";
+
+    if ($stmt->affected_rows === -1) {
+        echo "<script>alert('Edit Not Success!');</script>";
         die();
     }
+
     header("location: student.php");
-        die();
+    exit;
 }
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -98,6 +95,8 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
             </div>
             <a href="student.php"><i class="fas fa-arrow-alt-circle-left"></i></a>
             <form class="am-body-box" action="editStudent.php" autocomplete="off" method="post" id="timeServiceForm">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
+
                 <div class="am-row">
                     <div class="am-col-12">
                         <p>First Name:</p>
@@ -116,7 +115,6 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
                         <input type="text" name="lastname" id="lastname" value="<?php echo $lastname; ?>" required>
                     </div>
                 </div>
-
                 <div class="am-row">
                     <div class="am-col-12">
                         <p>Address:</p>
@@ -127,7 +125,6 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
                     <div class="am-col-12">
                         <p>Class:</p>
                         <select name="class_id" id="class_id" class="class_id" required>
-                            <option></option>
                             <?php 
                             $classes = $connect->query("SELECT * FROM classes order by levelsection asc ");
                             while($row = $classes->fetch_array()):
