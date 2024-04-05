806b6261c2da2c62b8bddc15ca7d3877692b8acd
Kris Nathaniel Dimaapi <21-00385@g.batstate-u.edu.ph>
2024-04-05 19:48:29 +0800

    added school organization

--- student-portal/index.php
+++ student-portal/index.php
@@ -386,11 +386,8 @@
                     <img src="images/blog-1.jpg" alt="">
                 </div>
                 <div class="content">
-                    <div class="icons">
-                        <a href="#"><i class="fas fa-user"></i></a>
-                    </div>
-                    <h3>Certificate of <br>Enrollment</h3>
-                    <p>Confirms current enrollment in an educational institution.</p>
+                    <h3>Certificate of Enrollment</h3>
+                    <p>Confirms current enrollment in an educational <br>institution.</p>
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSf2pzaZgvqPz0GePHIZtRFSxgyXlFOl5lZimjMzdSttEf11_A/viewform" class="btn">
                         <span class="text text-1">Get Form</span>
                         <span class="text text-2" aria-hidden="true">Get Form</span>
@@ -403,11 +400,8 @@
                     <img src="images/blog-2.jpg" alt="">
                 </div>
                 <div class="content">
-                    <div class="icons">
-                        <a href="#"><i class="fas fa-user"></i></a>
-                    </div>
                     <h3>Certificate of Graduation</h3>
-                    <p>Validates successful completion of an academic program.</p>
+                    <p>Validates successful completion of an academic <br>program.</p>
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSflI3eIW4UKiTDCbnIxPFMOOt6PTrLld-KRu5shcD47_F-n1A/viewform" class="btn">
                         <span class="text text-1">Get Form</span>
                         <span class="text text-2" aria-hidden="true">Get Form</span>
@@ -420,9 +414,6 @@
                     <img src="images/blog-5.jpg" alt="">
                 </div>
                 <div class="content">
-                    <div class="icons">
-                        <a href="#"><i class="fas fa-user"></i></a>
-                    </div>
                     <h3>Certificate of Good Moral</h3>
                     <p>Affirmation of individual's good moral character for documentation.</p>
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSeiXOWpTUuUR9UxahUWh69ncPRRXGnXijGATQiGw9bn479PgA/viewform" class="btn">
@@ -436,9 +427,6 @@
                     <img src="images/blog-4.jpg" alt="">
                 </div>
                 <div class="content">
-                    <div class="icons">
-                        <a href="#"><i class="fas fa-user"></i></a>
-                    </div>
                     <h3>Certificate of English Medium</h3>
                     <p>English language instruction proof for international education settings.</p>
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLScwi1q4i3a7ZpP6z3Kvv--ppmnMMpfATJ1eH5RS8ZDMnM4UsA/viewform" class="btn">
@@ -452,9 +440,6 @@
                     <img src="images/blog-3.jpg" alt="">
                 </div>
                 <div class="content">
-                    <div class="icons">
-                        <a href="#"><i class="fas fa-user"></i></a>
-                    </div>
                     <h3>Certificate of Subject Non-Availabilty</h3>
                     <p>Certificate confirming subject unavailability for academic purposes.</p>
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSdK7LjRs310yRlBGi-tLyOqEzV4jWv3jnERt-6K0Zo4JYKicg/viewform" class="btn">
@@ -469,7 +454,181 @@
     </section>
 
     <!-- blog section ends -->
+    <section class="blog" id="blog">
 
+<h1 class="heading">School Organization</h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <div class="image shine">
+            <img src="images/ssg.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>Supreme Secondary Learner Government</h3>
+            <p>The Supreme Secondary Learner Government of Bauan Technical Integrated High School serves as the highest governing body for students within the school, responsible for overseeing student affairs, advocating for their interests, and promoting a positive school environment.  Through open communication and collaboration with the student body, the SSG fosters a sense of community.</p>
+            <a href="https://www.facebook.com/SSLGBTIHS?mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image shine">
+            <img src="images/btihs.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>BTIHS Interact Club</h3>
+            <p>The BTIHS Interact Club is a student-led organization affiliated with Rotary International, dedicated to promoting community service, leadership development, and international understanding among its members. 
+                Through various service projects and activities, the club aims to make a positive impact both locally and globally while providing opportunities for students to grow as leaders and engage with their community.</p>
+            <a href="https://www.facebook.com/InteractBTIHS?mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image shine">
+            <img src="images/redcross.jpg" alt="">
+        </div>
+        <div class="content">
+
+            <h3>Red Cross Youth</h3>
+            <p>The Red Cross Youth (RCY) is a student-led organization dedicated to promoting humanitarian values, community service, and emergency response preparedness. 
+                Through various activities and projects, RCY members work to make a positive impact on their communities by providing assistance during disasters, promoting health and safety, and advocating for humanitarian causes.</p>
+            <a href="https://www.facebook.com/profile.php?id=61550077271878&mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="image shine">
+            <img src="images/kabayani.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>Kabayani Club</h3>
+            <p>The Kabayani Club is a student organization committed to celebrating Filipino culture, fostering camaraderie, and promoting civic engagement. Through cultural events, community service projects, and educational initiatives, the club strives to instill pride in Filipino heritage, while also encouraging members to contribute positively to society.  By embracing Filipino identity and social responsibility, the Kabayani Club empowers its members to become well-rounded and active citizens.</p>
+            <a href="https://www.facebook.com/profile.php?id=61553370361810&mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="image shine">
+            <img src="images/YES.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>Youth for the Environment in Schools Organization</h3>
+            <p>YES (Youth for the Environment in Schools Organization) is a student-led club dedicated to environmental conservation, sustainability education, and community action. Through projects, workshops, and awareness campaigns, YES empowers youth to become stewards of the environment, advocating for positive change and fostering a sense of responsibility towards our planet. YES inspires young minds to create a more sustainable future.</p>
+            <a href="https://www.facebook.com/YESOBTIHS?mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="image shine">
+            <img src="images/bricks.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>The Bricks</h3>
+            <p>"The Bricks" is the official publication of Bauan Technical Integrated High School, dedicated to providing insightful journalism, creative expression, and informative content to the school community. Through articles, features, and editorials, "The Bricks" aims to inform, inspire, and engage readers while showcasing the talents and voices of students and staff, fostering a culture of open communication and a vibrant school environment.</p>
+            <a href="https://www.facebook.com/BTIHSTHEBRICKS?mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="image shine">
+            <img src="images/teen.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>Teen Health Kiosk</h3>
+            <p>The Teen Health Kiosk at Bauan Technical Integrated High School is a vital resource center dedicated to promoting physical, mental, and emotional well-being among students. The kiosk offers confidential support, resources, and information on various health topics relevant to teenagers. From sexual health education to mental health awareness, the Teen Health Kiosk provides a safe and accessible space for students to seek guidance and support for their health needs. The kiosk serves as a valuable resource for parents, teachers, and counselors seeking to better understand and address the health concerns of adolescents.</p>
+            <a href="https://www.facebook.com/BTIHSTHK?mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="image shine">
+            <img src="images/kpsep.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>Kabataang Pampaaralang Sektor sa Edukasyon sa Pagpapakatao (KPSEP)</h3>
+            <p>The Kabataang Pampaaralang Sektor sa Edukasyon sa Pagpapakatao (KPSEP) Club is a religious organization at our school devoted to nurturing spiritual growth, moral values, and ethical principles among students. Through prayer meetings, fellowship activities, and service projects, the club seeks to inspire a deeper understanding of faith and promote positive character development. KPSEP provides a supportive community where students can explore their spirituality 
+                and uphold principles of kindness, compassion, and integrity.</p>
+            <a href="https://www.facebook.com/KPSEPCLUB?mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="image shine">
+            <img src="images/scout.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>Boy Scouts of the Philippines</h3>
+            <p>The BSP (Boy Scouts of the Philippines) Club at Bauan Technical Integrated High School is a youth organization dedicated to instilling leadership skills, outdoor proficiency, and civic-mindedness among its members. Through activities such as camping, community service projects, and skills training, the BSP Club aims to prepare students for responsible citizenship while fostering camaraderie and teamwork. With a focus on the Scout Law and Scout Oath, the club promotes values of integrity, loyalty, and service to others, empowering students to become well-rounded individuals 
+                and future leaders in their communities.</p>
+            <a href="https://www.facebook.com/TechKnightsScouts?mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="image shine">
+            <img src="images/math.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>Math Club</h3>
+            <p>The Math Club at BTIHS is a dynamic student organization devoted to fostering a passion for mathematics and enhancing problem-solving skills among its members. Through engaging activities, competitions, and workshops, the club provides opportunities for students to explore the beauty and relevance of mathematics beyond the classroom. Whether preparing for math competitions or delving into advanced mathematical concepts, the Math Club promotes collaboration, critical thinking, and a love for learning, 
+                empowering students to excel in the field of mathematics and beyond.</p>
+            <a href="https://www.facebook.com/MathemaTechnites.Lodge?mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="image shine">
+            <img src="images/english.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>English Club</h3>
+            <p>The English Club at BTIHS is a vibrant community where students come together to celebrate and enhance their proficiency in the English language. Through various activities such as debates, public speaking events, literary discussions, and creative writing workshops, the club fosters language fluency, communication skills, and a love for literature among its members. With a focus on language enrichment and cultural appreciation, the English Club provides a supportive environment for students to express themselves confidently and develop lifelong skills for academic and professional success.</p>
+            <a href="https://www.facebook.com/btihsEnglishClub?mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="image shine">
+            <img src="images/filipino.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>Filipino Club</h3>
+            <p>he Filipino Club at BTIHS is a lively organization dedicated to celebrating Filipino culture, language, and traditions. Through cultural events, language lessons, and community outreach projects, the club aims to promote pride in Filipino heritage and foster camaraderie among its members. From showcasing traditional dances and music to exploring the rich history and literature of the Philippines, the Filipino Club provides a platform for students to connect with their roots and deepen their appreciation for Filipino identity, cultivating a lasting sense of belonging.</p>
+            <a href="https://www.facebook.com/btihsAKBAY?mibextid=LQQJ4d" class="btn">
+                <span class="text text-1">Visit Page</span>
+                <span class="text text-2" aria-hidden="true">Visit Page</span>
+            </a>
+        </div>
+    </div>
+
+
+</div>
+
+</section>
 
     <!-- footer section stars -->
 
